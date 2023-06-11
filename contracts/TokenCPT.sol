fe05a58b9c3d89a7abb76a2bae0e996dd27a60f9
Wasi <k191357@nu.edu.pk>
2023-06-12 02:06:01 +0500

    added local and mumnai test network
    there is a little problem whole deploying check on that later

--- contracts/TokenCPT.sol
+++ contracts/TokenCPT.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CyberPuffT is ERC20, ERC20Burnable, Ownable {
+contract TokenCPT is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("CyberPuffT", "CPT") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
