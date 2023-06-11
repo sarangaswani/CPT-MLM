338706ace0c9936de9393674d327ea363faf26c9
FocusBT <k191357@nu.edu.pk>
2023-06-12 01:27:02 +0500

    Added Home
    Added Home and NavBar, next task is to add smart contract and the integration with metamask and trust wallet

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/HeroSection/Hero'
-import Card from './Components/HeroSection/Card';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
-import LoginForm from './Components/Register/Login';
-import Home from './Components/HeroSection/Home';
-import RegisterForm from './Components/Register/Register';
-import CompanyOverview from './Components/CompanyOverview/CompanyOverview';
-import CoreValuesSlider from './Components/CoreValues/CoreValueSlider';
-import Dashboard from './Components/Dashboard/Dashboard';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/HeroSection/Hero";
+import Card from "./Components/HeroSection/Card";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./Components/Register/Login";
+import Home from "./Components/HeroSection/Home";
+import RegisterForm from "./Components/Register/Register";
+import CompanyOverview from "./Components/CompanyOverview/CompanyOverview";
+import CoreValuesSlider from "./Components/CoreValues/CoreValueSlider";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-     {/* <Navbar/> */}
-     {/* <Home/>
-     <CompanyOverview/>
-     <CoreValuesSlider/>
-     <RegisterForm/> */}
-     <Dashboard/>
-     <Routes>
-     {/* <Route exact path='/Login' element={<RegisterForm/>}/>   */}
-     {/* <Route exact path='/' element={<Home/>}/>   */}
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Home />
+        <CompanyOverview />
+        <CoreValuesSlider />
+        <RegisterForm />
+        <Dashboard />
+        <Routes>
+          {/* <Route exact path='/Login' element={<RegisterForm/>}/>   */}
+          {/* <Route exact path='/' element={<Home/>}/>   */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
