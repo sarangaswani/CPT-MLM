d82594c853141345e0b0ca4208c662898077c6bd
Wasi <k191357@nu.edu.pk>
2023-06-17 17:59:08 +0500

    .

--- contracts/TokenCPT.sol
+++ contracts/TokenCPT.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+//
+
 contract TokenCPT is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("CyberPuffT", "CPT") {}
 
